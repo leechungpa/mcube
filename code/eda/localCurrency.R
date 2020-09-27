b153143fa88d6f519d4193c2df10b9277172acfc
leechungpa <leechungpa@naver.com>
2020-09-28 02:10:03 +0900

    eda 수정

--- code/eda/localCurrency.R
+++ code/eda/localCurrency.R
@@ -2,7 +2,7 @@ getwd()
 
 library(tidyverse)
 
-data=read_csv('./data/general_currency_weekly.csv')
+data=read_csv('./data/localcurrency/general_currency_weekly.csv')
 
 
 colnames(data)
@@ -15,7 +15,10 @@ data %>% filter(결제상품명 != '양평농협 창립50주년 기프트카드'
   facet_wrap(~결제상품명, scales = "free", ncol=5)+
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
-        axis.ticks.x=element_blank())
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
 
 
 
@@ -24,3 +27,25 @@ data %>% filter(결제상품명 != '양평농협 창립50주년 기프트카드'
   geom_col()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
+#############
+
+
+
+min(data$weekly_from_mon)
+max(data$weekly_from_mon)
+
+
+data %>% 
+  # filter(결제상품명 == '부천페이') %>% 
+  group_by(weekly_from_mon, 결제상품명 ) %>%
+  summarize(total=sum(결제총액)) %>%
+  ggplot(aes(x=weekly_from_mon , y=total, col = 결제상품명))+
+  geom_line()
+  
+
+
+
+data %>% filter(결제상품명 != '양평농협 창립50주년 기프트카드') %>%
+  ggplot(aes(x=가맹점업종명, y=결제총액, col=가맹점업종명))+
+  geom_line()+
+  facet_wrap(~결제상품명, scales = "free", ncol=5)
